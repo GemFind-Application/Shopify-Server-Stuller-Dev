9f98309ae7af299e78440f8a94006931e8139b27
Somin Parate <somin.parate@gmail.com>
2024-11-21 19:03:43 +0530

    uploading source code

---
+++ application/views/footer.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$shop = $_GET['shop'];?>
+<span class="gemfind_powered_by"><a href="https://gemfind.com/" target="_blank" rel="nofollow">Powered By GemFind</a></span>
+<input type="hidden" name="gemfind_stuller_version" value="1.0" />
+
---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+// fonts.php
+//header("Access-Control-Allow-Origin: *");
+//header("Content-Type: application/font-woff2");
+//echo @file_get_contents("https://shopifygemtools.gemfind.com/diamondlink/assets/fonts/Luma-Icons.woff");
+//echo @file_get_contents("https://shopifygemtools.gemfind.com/diamondlink/assets/fonts/Luma-Icons.woff2");
+//echo @file_get_contents("https://shopifygemtools.gemfind.com/diamondlink/assets/fonts/Luma-Icons.ttf");
+?>
